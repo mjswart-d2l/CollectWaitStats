be826214178940aee1c22957f4ecfb2d938cb7e2
h <h>
2021-01-13 17:07:20 +0000

    added Job Description

--- LatchStats/4. Job CollectLatchStats.sql
+++ LatchStats/4. Job CollectLatchStats.sql
@@ -1,22 +1,27 @@
 DECLARE @DBName sysname = DB_NAME();
 DECLARE @JobName sysname = 'CollectLatchStats';
+DECLARE @JobDesc sysname
+    = 'Source: ' + CHAR(9) + 'https://github.com/mjswart/CollectWaitStats.' 
+	+ 'Created: '+ CHAR(9) + CAST(GETDATE() AS VARCHAR(20))
+    + 'By: '+ CHAR(9) + SUSER_NAME();
 
 IF NOT EXISTS 
 (
 	SELECT *
-	FROM msdb.dbo.sysjobs
-	WHERE name = @JobName
+FROM msdb.dbo.sysjobs
+WHERE name = @JobName
 )
 BEGIN
 	EXEC msdb.dbo.sp_add_job 
-		@job_name = @JobName;
+		@job_name = @JobName,
+		@description = @JobDesc;
 
 	EXEC msdb.dbo.sp_add_jobstep 
 		@job_name = @JobName,
 		@step_name = N'Collect Latch Stats',
 		@command = N'exec s_CollectLatchStats @WaitThresholdInMs = 100;', 
 		@database_name = @DBName;
-	
+
 	EXEC msdb.dbo.sp_add_jobserver 
 		@job_name = @JobName,
 		@server_name = N'(local)';
--- SpinStats/4. Job CollectSpinStats.sql
+++ SpinStats/4. Job CollectSpinStats.sql
@@ -1,22 +1,27 @@
 DECLARE @DBName sysname = DB_NAME();
 DECLARE @JobName sysname = 'CollectSpinStats';
+DECLARE @JobDesc sysname
+    = 'Source: ' + CHAR(9) + 'https://github.com/mjswart/CollectWaitStats.' 
+	+ 'Created: '+ CHAR(9) + CAST(GETDATE() AS VARCHAR(20))
+    + 'By: '+ CHAR(9) + SUSER_NAME();
 
 IF NOT EXISTS 
 (
 	SELECT *
-	FROM msdb.dbo.sysjobs
-	WHERE name = @JobName
+FROM msdb.dbo.sysjobs
+WHERE name = @JobName
 )
 BEGIN
 	EXEC msdb.dbo.sp_add_job 
-		@job_name = @JobName;
+		@job_name = @JobName,
+		@description = @JobDesc;
 
 	EXEC msdb.dbo.sp_add_jobstep 
 		@job_name = @JobName,
 		@step_name = N'Collect Spin Stats',
 		@command = N'exec s_CollectSpinStats @CollisionsThreshold = 100;', 
 		@database_name = @DBName;
-	
+
 	EXEC msdb.dbo.sp_add_jobserver 
 		@job_name = @JobName,
 		@server_name = N'(local)';
