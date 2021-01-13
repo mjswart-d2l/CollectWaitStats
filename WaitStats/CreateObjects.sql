fbe643ec3729c876b18230c30739ee25e3b3b7e8
h <h>
2021-01-13 17:43:39 +0000

    CreateObjects scripts to match

--- WaitStats/CreateObjects.sql
+++ WaitStats/CreateObjects.sql
@@ -151,23 +151,28 @@ FROM   WaitStatsHistogram;
 GO
 DECLARE @DBName sysname = DB_NAME();
 DECLARE @JobName sysname = 'CollectWaitStats';
+DECLARE @JobDesc sysname
+    = ' Source:   ' + 'https://github.com/mjswart/CollectWaitStats. ;' 
+	+ ' Created:   '+ CAST(GETDATE() AS VARCHAR(20)) + ';'
+    + ' By:    '+ SUSER_NAME() + ';';
 
 IF NOT EXISTS 
 (
 	SELECT *
-	FROM msdb.dbo.sysjobs
-	WHERE name = @JobName
+FROM msdb.dbo.sysjobs
+WHERE name = @JobName
 )
 BEGIN
 	EXEC msdb.dbo.sp_add_job 
-		@job_name = @JobName;
+		@job_name = @JobName,
+		@description = @JobDesc;
 
 	EXEC msdb.dbo.sp_add_jobstep 
 		@job_name = @JobName,
 		@step_name = N'Collect Wait Stats',
 		@command = N'exec s_CollectWaitStats @WaitThresholdInMs = 1000;', 
 		@database_name = @DBName;
-	
+
 	EXEC msdb.dbo.sp_add_jobserver 
 		@job_name = @JobName,
 		@server_name = N'(local)';
